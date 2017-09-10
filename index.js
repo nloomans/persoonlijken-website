5b99051278b006adfaa1f630fb5da33fa68f53f0
Noah Loomans <noahloomans@gmail.com>
2017-09-10 15:55:00 +0200

    Improve progressBar function

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var elem = document.getElementById("myBar");
-var width = 1;
-var id = setInterval(frame, 10);
-function frame() {
-  if (width >= 100) {
-    clearInterval(id);
-  } else {
-    width++;
-    elem.style.width = width + '%';
+function progressBar(elem, finalWidth) {
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= finalWidth) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+    }
   }
 }
+
+progressBar(document.getElementById('myBar'), 50);
+progressBar(document.getElementById('myBar1'), 75);
