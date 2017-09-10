c800f6322f5217a3c3e63d4ad1133bca27c499ed
Bilal Ayachi <ayachi-code@users.noreply.github.com>
2017-09-10 14:49:02 +0200

    Add files via upload
    Test 112233

---
+++ index persoonlijke website 2.0/index.js
@@ -0,0 +1,13 @@
+function move() {
+  var elemf = document.getElementById("html_data_bar");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+    }
+  }
+}
